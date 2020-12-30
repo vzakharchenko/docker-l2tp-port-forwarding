e1607d2d887cce3abfc40fac1ddc892efdec23ad
vzakharchenko <vzakharchenko@modeln.com>
2020-12-30 20:14:22 +0200

    fix

--- docker.publish.sh
+++ docker.publish.sh
@@ -1,6 +1,6 @@
 docker build -t l2tp-port-forwarding .
-docker tag  l2tp-port-forwarding vassio/l2tp-port-forwarding:1.0.0
-docker push vassio/l2tp-port-forwarding:1.0.0
+docker tag  l2tp-port-forwarding vassio/l2tp-port-forwarding:1.0.1
+docker push vassio/l2tp-port-forwarding:1.0.1
 
 docker tag  l2tp-port-forwarding vassio/l2tp-port-forwarding:latest
 docker push vassio/l2tp-port-forwarding:latest
